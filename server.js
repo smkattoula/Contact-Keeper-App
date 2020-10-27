25bb56f45118f29e08fdc30cb39c2592479edbed
Shaker Kattoula <smkattoula@gmail.com>
2020-10-27 14:18:22 -0700

    create User model and validations for register user route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Middleware
+app.use(express.json({ extended: false }));
+
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
