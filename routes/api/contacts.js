f8798829c673b0ceac62899beca62433f938fab5
Shaker Kattoula <smkattoula@gmail.com>
2020-10-27 17:07:51 -0700

    create private routes to add, update, and delete contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -57,15 +57,60 @@ router.post(
 // Route: PUT api/contacts/:id
 // Description: Update a contact
 // Access: Private
-router.put("/:id", auth, (req, res) => {
-  res.send("Update a contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Validation to make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // Route: DELETE api/contacts/:id
 // Description: Delete a contact
 // Access: Private
-router.delete("/:id", auth, (req, res) => {
-  res.send("Delete a contact ");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Validation to make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Contact removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
